f1a8208273a56606ad4875cf85cad80a7025023c
kawakuti code <kawakuticode@gmail.com>
2020-11-13 04:07:41 +0100

    module radio app developed update local database url .

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-"in future will use env variables for config"
-
+import os
 DATABASE_URI = 'postgres://kawakuticode@localhost:5432/angola_web'
-PROD_DATABASE_URI = 'postgres://kawakuticode@localhost:5432/angola_web'
+PROD_DATABASE_URI = 'postgres://wsteasfakevqhq:3cae8bc967753884fde1db46469c5736064a47813a9923327126e60668d11aa2@ec2-54-247-94-127.eu-west-1.compute.amazonaws.com:5432/d13tl0r9i43etv'
 DEV_DATABASE_URI = 'postgres://kawakuticode@localhost:5432/angola_web'
 
 
@@ -9,7 +8,7 @@ class Config:
     """Base config."""
     # SECRET_KEY = environ.get('SECRET_KEY')
     # SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
-
+    SECRET_KEY = os.urandom(32)
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
     SQLALCHEMY_DATABASE_URI = DATABASE_URI
@@ -18,6 +17,8 @@ class Config:
 
 
 class ProdConfig(Config):
+
+    SECRET_KEY = os.urandom(32)
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
@@ -29,6 +30,8 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
+
+    SECRET_KEY = os.urandom(32)
     FLASK_ENV = 'development'
     DEBUG = True
     TESTING = True
