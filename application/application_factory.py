bec12804b7e26846884f56559e35407c85a04405
kawakuti code <kawakuticode@gmail.com>
2021-01-14 20:34:30 +0100

    fixed bug to order extended forecast by date

--- application/application_factory.py
+++ application/application_factory.py
@@ -12,7 +12,7 @@ def create_app():
     """Initialize the core application."""
 
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.DevConfig')
+    app.config.from_object('config.ProdConfig')
     register_scheduler(app)
 
     db.init_app(app)
